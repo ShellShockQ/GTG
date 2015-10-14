d98ff52cacfa8c7171f6ab104f3a73bab2d4e6ec
Detrick DeBurr <detrick@digitalflow.com>
2015-10-14 17:10:17 -0500

    Pulling back the charity detail information

--- app/src/main/java/com/gametimegiving/mobile/Charity.java
+++ app/src/main/java/com/gametimegiving/mobile/Charity.java
@@ -8,9 +8,13 @@ public class Charity {
     List<Charity> listOfCharities;
     private int CharityId;
     private String CharityName;
-    private String Summary;
-    private String Description;
-    private Bitmap logo;
+    private String Detail;
+    private String Mission;
+    private String Purpose;
+    private String ContactEmail;
+    private String ContactPhone;
+    private String Logo;
+    private Bitmap bitmap;
 
     public int getCharityId() {
         return CharityId;
@@ -28,28 +32,60 @@ public class Charity {
         this.CharityName = teamName;
     }
 
-    public String getSummary() {
-        return Summary;
+
+    public String getDetail() {
+        return Detail;
     }
 
-    public void setSummary(String summary) {
-        this.Summary = summary;
+    public void setDetail(String detail) {
+        Detail = detail;
     }
 
-    public Bitmap getLogo() {
-        return logo;
+    public String getMission() {
+        return Mission;
     }
 
-    public void setLogo(Bitmap logo) {
-        this.logo = logo;
+    public void setMission(String mission) {
+        Mission = mission;
     }
 
-    public String getDescription() {
-        return Description;
+    public String getPurpose() {
+        return Purpose;
     }
 
-    public void setDescription(String description) {
-        Description = description;
+    public void setPurpose(String purpose) {
+        Purpose = purpose;
     }
 
+    public String getContactEmail() {
+        return ContactEmail;
+    }
+
+    public void setContactEmail(String contactEmail) {
+        ContactEmail = contactEmail;
+    }
+
+    public String getContactPhone() {
+        return ContactPhone;
+    }
+
+    public void setContactPhone(String contactPhone) {
+        ContactPhone = contactPhone;
+    }
+
+    public String getLogo() {
+        return Logo;
+    }
+
+    public void setLogo(String logo) {
+        Logo = logo;
+    }
+
+    public Bitmap getBitmap() {
+        return bitmap;
+    }
+
+    public void setBitmap(Bitmap bitmap) {
+        this.bitmap = bitmap;
+    }
 }
